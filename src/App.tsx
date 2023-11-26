48a86703cee3a08c685492ece567243cf14d691b
gavryk <gvozdyk23@gmail.com>
2023-11-27 01:55:27 +0200

    test: api conections with rtk query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
+import { useLoginUserMutation } from './store/api/auth.api';
+
 const App: React.FC = () => {
+	const [loginUser, { data: userData, isLoading: loginLoading }] = useLoginUserMutation();
+
+	const logIn = async () => {
+		await loginUser({
+			email: 'gvozdyk23@gmail.com',
+			password: '1993099a',
+		});
+	};
+
+	useEffect(() => {
+		if (userData) {
+			console.log(userData.user);
+		}
+	}, [userData]);
+
 	return (
 		<>
-			<h1>New Project Tasks Manager</h1>
+			<h1>Hello {userData ? userData.user.name : 'unknown'}</h1>
+			<button onClick={logIn}>LogIn</button>
 		</>
 	);
 };
---
+++ src/store/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
+import { api } from './api/api';
+
+export const store = configureStore({
+	reducer: {
+		[api.reducerPath]: api.reducer,
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
