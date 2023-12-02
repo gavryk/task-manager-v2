2bf7300a680d9b9141b0c69249c87a672a014667
gavryk <gvozdyk23@gmail.com>
2023-12-02 17:36:43 +0200

    test: api conections with rtk query

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,7 @@ import { useEffect } from 'react';
 import { useLoginUserMutation } from './store/api/auth.api';
 
 const App: React.FC = () => {
-	return (
-		<>
-			<h1>Hello {userData ? userData.user.name : 'unknown'}</h1>
-			<button onClick={logIn}>LogIn</button>
-		</>
-	);
+	return <></>;
 };
 
 export default App;
